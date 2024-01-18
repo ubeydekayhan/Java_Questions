ee51b5dc9d6153bef4157b00a570ce18292ae063
Mustafa Ubeyde Kayhan <mubeydekayhan.42@gmail.com>
2024-01-18 21:05:56 +0300

    Q01

---
+++ src/main/java/beginner/variables/Q01.java
@@ -0,0 +1,16 @@
+package beginner.variables;
+
+public class Q01 {
+    /*
+    -Create variables of type double data for the prices of any 3 items.
+    -Print the sum of the prices to the console.
+    */
+    public static void main(String[] args) {
+
+        double shirt=3.10;
+        double hat=6.50;
+        double glove=2.60;
+
+        System.out.println("Sum Price"+(shirt+hat+glove));
+    }
+}
